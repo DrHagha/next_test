f5981ce1765c535cc24d2a396982931845109b3a
전민규 <jeonmingyu@jeonmingyuui-MacBookPro.local>
2023-07-19 16:46:35 +0900

    date모듈 import 대소문자 변경

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostData } from "../lib/posts";
-import Date from "../components/Date";
+import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
