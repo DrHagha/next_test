59d97f7386d64202a6959105285d9a64930cbd9c
전민규 <jeonmingyu@jeonmingyuui-MacBookPro.local>
2023-07-21 14:50:05 +0900

    페이지 링크 테스트 완료

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export const metadate = {
     title : `Next.js`,
 }
 
 export default function Page(){
     return (
-        <h1>hello world</h1>
+        <>
+            <h1>hello world</h1>
+            <Link href="/dashboard">Dashboard</Link>
+        </>
     );
 }
